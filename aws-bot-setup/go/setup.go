04e8fd7b00245b38359df587a471bdc462e04edd
Greg Uctum <greguctum@gmail.com>
2021-04-11 15:23:40 -0400

    setting up basic skeleton for go script to build AWS infra and create git repo

--- aws-bot-setup/go/setup.go
+++ aws-bot-setup/go/setup.go
@@ -2,9 +2,27 @@ package main
 
 import (
 	"fmt"
-	"github.com/pborman/uuid"
+	"log"
+	"os/exec"
 )
 
 func main() {
-	fmt.Println(uuid.NewRandom())
+	fmt.Println("Now creating the AWS infrastructure for the Discord bot")
+	// run cdk synth and cdk deploy in the infra-setup directory
+	cmd := exec.Command("cdk deploy")
+	cmd.Dir = "../infra-setup"
+	out, err := cmd.Output()
+	if err != nil {
+		log.Fatal(err)
+	} else  {
+		fmt.Printf("%s", out)
+	}
+	fmt.Println("AWS Infrastructure successfully created")
+	// create a git repo as well, see if we can share it on GitHub too
+	fmt.Println("Now creating git repository")
+	repoSetup()
+}
+
+func repoSetup() {
+
 }
