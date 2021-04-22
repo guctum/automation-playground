5b66e50b9f05976d670fa55ce59a7b8569ef1fe0
Greg Uctum <greguctum@gmail.com>
2021-04-22 12:18:00 -0400

    create t2.micro EC2 instance
    need to figure out how to associate a security key with it, as well as making sure this machine can be accessed

--- aws-bot-setup/aws-bot-infrastructure/aws_bot_infrastructure/aws_bot_infrastructure_stack.py
+++ aws-bot-setup/aws-bot-infrastructure/aws_bot_infrastructure/aws_bot_infrastructure_stack.py
@@ -4,7 +4,12 @@ from aws_cdk import core as cdk
 # the CDK's core module.  The following line also imports it as `core` for use
 # with examples from the CDK Developer's Guide, which are in the process of
 # being updated to use `cdk`.  You may delete this import if you don't need it.
-from aws_cdk import core
+from aws_cdk import (
+    aws_ec2 as ec2,
+    aws_iam as iam,
+    aws_s3 as s3,
+    core
+)
 
 
 class AwsBotInfrastructureStack(cdk.Stack):
@@ -14,3 +19,34 @@ class AwsBotInfrastructureStack(cdk.Stack):
 
         # The code that defines your stack goes here
 
+        bucket = s3.Bucket(self, "AWS Bot Bucket",
+                           versioned=True,
+                           removal_policy=core.RemovalPolicy.DESTROY,
+                           auto_delete_objects=True)
+
+        # VPC
+        vpc = ec2.Vpc(self, "VPC",
+                      nat_gateways=0,
+                      subnet_configuration=[ec2.SubnetConfiguration(name="public",subnet_type=ec2.SubnetType.PUBLIC)]
+                      )
+
+        # AMI
+        amzn_linux = ec2.MachineImage.latest_amazon_linux(
+            generation=ec2.AmazonLinuxGeneration.AMAZON_LINUX_2,
+            edition=ec2.AmazonLinuxEdition.STANDARD,
+            virtualization=ec2.AmazonLinuxVirt.HVM,
+            storage=ec2.AmazonLinuxStorage.GENERAL_PURPOSE
+        )
+
+        # Instance Role and SSM Managed Policy
+        role = iam.Role(self, "InstanceSSM", assumed_by=iam.ServicePrincipal("ec2.amazonaws.com"))
+
+        role.add_managed_policy(iam.ManagedPolicy.from_aws_managed_policy_name("service-role/AmazonEC2RoleforSSM"))
+
+        # Instance
+        instance = ec2.Instance(self, "Instance",
+                                instance_type=ec2.InstanceType("t2.micro"),
+                                machine_image=amzn_linux,
+                                vpc = vpc,
+                                role = role
+                                )
